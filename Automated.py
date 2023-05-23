d655e2e046788698c4440d97c11c414fd3537850
heederah <127055055+issabolanle@users.noreply.github.com>
2023-05-23 14:58:58 +0100

    Update Automated.py

--- Automated.py
+++ Automated.py
@@ -9,13 +9,13 @@ import time
 name = "nimah"
 address_sender = 'issabolanle44@gmail.com'
 password_sender =  'ldefuqondahlauxv'
-input_address_recipients = 'okulichrist@gmail.com, delaw34@yah00.com, mosesoleka@gmail.com, bukolainnocent66@gmail.com, callistus100@gmail.com'
-address_recipients = input_address_recipients.split(',')
+input_address_recipients = ['okulichrist@gmail.com', 'delaw34@yah00.com', 'mosesoleka@gmail.com', 'bukolainnocent66@gmail.com', 'callistus100@gmail.com']
+# address_recipients = input_address_recipients.split(',')
 subject = 'Automated Email'
 body =  'Hello how are you doing, this is an automated email sent every 10 minutes to remind you of your assignment submission before 7pm, pls deploy to your github account and send the link to the email. Thanks.'
+mail_time = datetime.datetime.now()
 
 email_msg = EmailMessage()
-
 email_msg['From'] = address_sender
 email_msg['To'] = ','.join(input_address_recipients)
 email_msg['Subject'] = subject
